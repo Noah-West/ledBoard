71bb4c58394df0ec0c92b8783de4d11608211e91
West <210731@yssd.org>
2022-02-09 14:56:33 -0500

    work on extracting functions

--- TKinterLED.py
+++ TKinterLED.py
@@ -3,41 +3,7 @@ import random
 import tkinter as tk
 import time
 import threading
-
-newKeys = []
-heldKeys = []
-keyLock = threading.Lock()
-
-def bttnPress(a):
-  with keyLock:
-    global heldKeys, newKeys
-    newKeys.append((int(a.x/50),int(a.y/50)))
-    heldKeys.append((int(a.x/50),int(a.y/50)))
-def bttnRelease(a):
-  with keyLock:
-    global heldKeys
-    heldKeys = []
-def readKeys():
-    """Returns newly pressed keys, as well as all keys being held
-    (List newKeys, List pressedKeys)"""
-    with keyLock:
-        global newKeys
-        locNewKeys = newKeys
-        newKeys = []
-        return(locNewKeys, heldKeys)
-    
-def drawGrid(grid):
-    for y, row in enumerate(grid):
-        for x, val in enumerate(row):
-            drawPixel(x,y,val)
-def drawPixel(x, y, c):
-    canvas.itemconfigure(bttnGrid[y][x], fill = "#{:06x}".format(c))
-def setCol(c = 0, n = range(384)):
-    for x in range(8):
-        for y in range(8):
-            drawPixel(x,y,c)
-def Color(r,g,b):
-    return (r<<16)+(g<<8)+b
+from tKinterGrid import *
 
 colors = []
 colors.append(0) #Black
@@ -277,15 +243,11 @@ def funcDispatch():
         print("exit mode {}".format(mode))
         mode = (mode+1)%len(modes)
         setCol()
-        global heldKeys, newKeys, keyLock
-        with keyLock:
-            heldKeys = []
-            newKeys = []
+        cleanupGrid()
         time.sleep(1) 
 
 
 if __name__ == '__main__':
-    global bttnGrid, canvas
     
     window = tk.Tk()
     window.title("Hello wold")
@@ -294,8 +256,8 @@ if __name__ == '__main__':
     bttnGrid = [[0 for x in range(8)] for y in range(8)]
     canvas = tk.Canvas(width = 400, height = 400, bg = "blue")
 
-    for y,row in enumerate(bttnGrid):
-        for x, val in enumerate(row):
+    for y in range(8):
+        for x in range(8):
             bttnGrid[y][x] = canvas.create_rectangle(x*50, y*50, x*50+50, y*50+50, fill='#000000', outline='#FFFFFF')
     canvas.bind("<Button-1>", lambda a: bttnPress(a))
     canvas.bind("<ButtonRelease-1>", lambda a: bttnRelease(a))
--- tKinterGrid.py
+++ tKinterGrid.py
@@ -1,13 +1,22 @@
 import time
+import math
+import random
+import tkinter as tk
+import threading
 
-def cleanup():
+newKeys = []
+heldKeys = []
+keyLock = threading.Lock()
+
+bttnGrid = [[]for i in range(8)]
+canvas
+def cleanupGrid():
     setCol()
-    global heldKeys, newKeys, keyLock
+    global heldKeys, newKeys
     with keyLock:
         heldKeys = []
         newKeys = []
     time.sleep(1) 
-
 def bttnPress(a):
   with keyLock:
     global heldKeys, newKeys
@@ -25,3 +34,36 @@ def readKeys():
         locNewKeys = newKeys
         newKeys = []
         return(locNewKeys, heldKeys)
+def drawGrid(grid):
+    for y, row in enumerate(grid):
+        for x, val in enumerate(row):
+            drawPixel(x,y,val)
+def drawPixel(x, y, c):
+    canvas.itemconfigure(bttnGrid[y][x], fill = "#{:06x}".format(c))
+def setCol(c = 0, n = range(384)):
+    for x in range(8):
+        for y in range(8):
+            drawPixel(x,y,c)
+def Color(r,g,b):
+    return (r<<16)+(g<<8)+b
+def startup():
+    global bttnGrid, canvas
+
+    window = tk.Tk()
+    window.title("Hello wold")
+    window.geometry("400x400")
+
+    bttnGrid = [[0 for x in range(8)] for y in range(8)]
+    canvas = tk.Canvas(width = 400, height = 400, bg = "blue")
+
+    for y in range(8):
+        for x in range(8):
+            bttnGrid[y][x] = canvas.create_rectangle(x*50, y*50, x*50+50, y*50+50, fill='#000000', outline='#FFFFFF')
+    canvas.bind("<Button-1>", lambda a: bttnPress(a))
+    canvas.bind("<ButtonRelease-1>", lambda a: bttnRelease(a))
+    canvas.pack()
+
+    #start  main functionality
+    tkThread = threading.Thread(target = tk.mainloop, daemon = True) 
+    tkThread.start()
+
